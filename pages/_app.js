54388168d76fc33917680a4afef207ecf2293094
maher <gomemahero.com>
2022-07-08 13:36:05 +0300

    install all packages and setup context and redux and chakra  ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
 import '../styles/globals.css'
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { wrapper } from "../store/index";
+import Context from '../context/index'
+import React, {useState, useEffect} from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);  
+
+  if (isSSR) return null;
+
+
+
+  return (
+    <React.Fragment>
+
+    <ChakraProvider>
+    
+    
+    <Context>
+    
+      {/* <Layout>  */}
+      <ToastContainer/>
+    
+    <Component {...pageProps} />
+    
+      {/* </Layout> */}
+   
+    </Context>
+    
+    </ChakraProvider>
+    </React.Fragment>
+  )
 }
 
-export default MyApp
+export default wrapper.withRedux( MyApp);
