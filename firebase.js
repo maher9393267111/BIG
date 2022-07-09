d4334dc88196d32c305752c6ed1fc9bd1b99b523
maher <gomemahero.com>
2022-07-10 01:47:15 +0300

    fix

--- firebase.js
+++ firebase.js
@@ -8,14 +8,22 @@ import { getAnalytics, logEvent } from "firebase/analytics"
 const firebaseConfig =  initializeApp({
    
 
-apiKey: "AIzaSyBI7p9VcmEmdtogk3GqjUNIrz_kNT1u14g",
-authDomain: "precise-reality-324621.firebaseapp.com",
-databaseURL: "https://precise-reality-324621-default-rtdb.firebaseio.com",
-projectId: "precise-reality-324621",
-storageBucket: "precise-reality-324621.appspot.com",
-messagingSenderId: "671790327953",
-appId: "1:671790327953:web:8e0f22b2ea69d2cc347730",
-measurementId: "G-TBEJLSK4SL"
+// apiKey: "AIzaSyBI7p9VcmEmdtogk3GqjUNIrz_kNT1u14g",
+// authDomain: "precise-reality-324621.firebaseapp.com",
+// databaseURL: "https://precise-reality-324621-default-rtdb.firebaseio.com",
+// projectId: "precise-reality-324621",
+// storageBucket: "precise-reality-324621.appspot.com",
+// messagingSenderId: "671790327953",
+// appId: "1:671790327953:web:8e0f22b2ea69d2cc347730",
+// measurementId: "G-TBEJLSK4SL"
+
+apiKey: "AIzaSyD_Eub_0yK8fx_ZuWvLTFu6S2bgVoYeB7M",
+authDomain: "mern-4dbe9.firebaseapp.com",
+projectId: "mern-4dbe9",
+storageBucket: "mern-4dbe9.appspot.com",
+messagingSenderId: "455005923505",
+appId: "1:455005923505:web:e91b064422d11f2222da90"
+
 
 
 
