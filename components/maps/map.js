166028806adbd4d3bfc2968c6abdd1d42076bea3
maher <gomemahero.com>
2022-07-11 17:20:05 +0300

    change image every 2second start work

--- components/maps/map.js
+++ components/maps/map.js
@@ -1,98 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { YMaps, Map, Clusterer, Placemark } from "react-yandex-maps";
 
-import React, {useState} from 'react';
+import POINTS from "./points";
 
-import {Button, Select} from "antd";
-import {YMaps, Map, Placemark, Clusterer} from 'react-yandex-maps';
-//import './styles/YandexMap.css'
-import points from './points.json'
-//import {Option} from "antd/es/mentions";
-const { Option } = Select;
 
-const MapPage = () => {
-    const pointsRegion = [
-        [45.059988, 38.957221],
-        [45.034228, 38.912224],
-        [45.072758, 39.038617],
-        [45.031483, 38.987719],
-        [45.049348, 39.031170],
-        [45.002002, 39.071999]
-    ]
 
-    const [coord, setCoord]=useState([45.031483, 38.987719])
-    const mapState = {
-        center: coord,
-        zoom: 15,
-        behaviors: ["default", "scrollZoom"]
-    };
-    const onChangeRegion = (val)=>{
-        setCoord(pointsRegion[parseInt(val)])
-    }
-
-    const getPointData = (index) => {
-        return {
-            balloonContentBody: "Магнит. Кэшбэк: <strong>" + "5%" + "</strong>",
-            clusterCaption: "Магнит <strong>" + index + "</strong>"
-        };
-    };
-
-    const getPointOptions = () => {
-        return {
-            preset: "islands#violetIcon"
-        };
-    };
-
-    return (
-        <div className='map_custom'>
-            <h5 className='map_txt'>Ближайшие магазины в
-                <Select
-                    defaultValue='Центральном микрорайоне'
-                    onChange={(value)=>onChangeRegion(value)}
-                    style={{
-                        marginLeft: 10
-                    }}
-                >
-                    <Option value={'0'}>Фестивальном микрорайоне</Option>
-                    <Option value={'1'}>Юбилейном микрорайоне</Option>
-                    <Option value={'2'}>Губернском микрорайоне</Option>
-                    <Option value={'3'}>Центральном микрорайне</Option>
-                    <Option value={'4'}>микрорайоне Панорама</Option>
-                    <Option value={'5'}>микрорайоне Гидростроителей</Option>
-                </Select>
-            </h5>
-            <div className='MapYandex'>
-                <YMaps>
-                    <Map
-                        state={mapState}
-                        width='100%'
-                        height='100%'>
-                        <Clusterer
-                            options={{
-                                preset: "islands#invertedVioletClusterIcons",
-                                groupByCoordinates: false,
-                                clusterDisableClickZoom: true,
-                                clusterHideIconOnBalloonOpen: false,
-                                geoObjectHideIconOnBalloonOpen: false
-                            }}
-                            modules={[
-                                'geoObject.addon.balloon',
-                                'geoObject.addon.hint'
-                            ]}
-                        >
-                            {points.map((coordinates, idx) => (
-                                <Placemark
-                                    key={idx}
-                                    geometry={coordinates}
-                                    properties={getPointData(idx)}
-                                    options={getPointOptions()}
-                                />
-                            ))}
-                        </Clusterer>
-                    </Map>
-
-                </YMaps>
-            </div>
-        </div>
-    );
+const mapState = {
+  center: [55.751574, 37.573856],
+  zoom: 5
 };
 
-export default MapPage;
+class MapComponent extends React.Component {
+  state = {
+    selectedPoint: null
+  };
+
+  onPlacemarkClick = point => () => {
+    this.setState({ selectedPoint: point });
+  };
+
+  render() {
+    const { selectedPoint } = this.state;
+    return (
+      <div className="App ">
+        <YMaps style={{display:'hidden'}}>
+          <Map defaultState={mapState}>
+            <Clusterer
+              options={{
+                preset: "islands#invertedVioletClusterIcons",
+                groupByCoordinates: false
+              }}
+            >
+              {POINTS.map((point, index) => (
+                <Placemark
+                  key={index}
+                  geometry={point.coords}
+                  onClick={this.onPlacemarkClick(point)}
+                />
+              ))}
+            </Clusterer>
+          </Map>
+        </YMaps>
+        {selectedPoint && (
+          <div>
+            <h1>Selected point: {selectedPoint.title}</h1>
+            <p>{selectedPoint.descr}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+
+export default MapComponent;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,46 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Box } from '@chakra-ui/react'
 import MapComponent from '../components/maps/map'
+import dynamic from 'next/dynamic';
+import {useState,useEffect} from 'react'
+
+
+
+
 export default function Home() {
+
+const [image,setImage] = useState('https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png')
+
+const images = [
+'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png',
+'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png',
+'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png',
+
+
+]
+
+
+// change image  every 5s
+useEffect(() => {
+ 
+  changeimage()
+  return () => clearInterval(changeimage);
+}, [])
+
+
+
+ const changeimage = () => {
+
+  setInterval(() => {
+       
+      setImage(images[Math.floor(Math.random() * images.length)]);
+  }
+  , 2000);
+ }
+
+
+
+
   return (
     <div className={styles.container}>
         <NextHead
@@ -14,10 +53,14 @@ export default function Home() {
 
 <div>
 
+
+<img className='h-[200px] w-[200px] my-12' src={image} alt="" />
+
+
 </div>
 
 
-<MapComponent />
+
 
 
 
