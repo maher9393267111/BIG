7a0ca9cf25655d8dfa822a712dffd109e8713f71
maher <gomemahero.com>
2022-07-11 18:59:35 +0300

    login page maked

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import NextHead from "../components/global/NextHead";
+import NextHead from "../../components/global/NextHead";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
 import { Box } from "@chakra-ui/react";
-import MapComponent from "../components/maps/map";
+
 import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
-import { Placemark } from "react-yandex-maps";
+
 import Link from "next/link";
 
 export default function Home() {
@@ -35,18 +35,16 @@ export default function Home() {
   const [formData, setformData] = useState({
     email: "",
     password: "",
-    userName: "",
-    fullName: "",
-    phone: "",
+  
   });
 
-  const { password, email, userName, fullName, phone } = formData;
+  const { password, email} = formData;
 
   const onChange = (e) =>
     setformData({ ...formData, [e.target.name]: e.target.value });
 
   return (
-    <div className={styles.container}>
+    <div className=''>
       <NextHead
         title="Home"
         metaDescription="Audiophile is the premier store for high end headphones, earphones, speakers, and accessories. Browse our collection."
@@ -73,7 +71,7 @@ export default function Home() {
           {/* -------Right Form---- */}
           <div>
             {/* ----form--- */}
-            <div className="w-[372px] h-[622px] border-2 border-blue-400">
+            <div className="w-[372px] h-[477px] border-2 border-blue-400">
               {/* ----all conten wrapper---- */}
               <div className=" container">
                 {/* ----insta logo--- */}
@@ -92,7 +90,7 @@ export default function Home() {
                 <div className=" container w-[277px] ml-12   mt-8">
                   <div>
                     <p className=" text-center text-[18px] font-semibold">
-                      Sign up to see photos and videos from your friends.
+                      Log in to see photos and videos from your friends.
                     </p>
                   </div>
 
@@ -106,7 +104,7 @@ export default function Home() {
                           src="https://cdn4.iconfinder.com/data/icons/socialcones/508/Google-256.png"
                           alt=""
                         />
-                        Sign In with Google{" "}
+                        Login In with Google{" "}
                       </button>
                     </div>
                   </div>
@@ -135,27 +133,9 @@ export default function Home() {
                           />
                         </div>
 
-                        <div className="my-[6px]">
-                          <input
-                            onChange={(e) => onChange(e)}
-                            name="fullName"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight    focus:outline-slate-400 focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            placeholder="fullName"
-                          />
-                        </div>
+                     
 
-                        <div className="my-[6px]">
-                          <input
-                            onChange={(e) => onChange(e)}
-                            name="userName"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight    focus:outline-slate-400 focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            placeholder="userName"
-                          />
-                        </div>
+                       
 
                         <div className="my-[6px]">
                           <input
@@ -168,12 +148,7 @@ export default function Home() {
                           />
                         </div>
 
-                        <div className="my-4 text-center font-semibold">
-                          <h1>
-                            By signing up, you agree to our Terms , Data Policy
-                            and Cookies Policy .
-                          </h1>
-                        </div>
+                       
 
                         {/* ----register button---- */}
 
@@ -182,19 +157,19 @@ export default function Home() {
                             <button
                               type="submit"
                               className={`  ${
-                                fullName === "" ||
+                                
                                 email === "" ||
                                 password === ""
                                   ? "opacity-[0.5]"
                                   : "opacity-1"
                               }   bg-blue-500 text-white font-bold p-2 rounded-md  block w-full `}
                               disabled={
-                                fullName === "" ||
+                               
                                 email === "" ||
                                 password === ""
                               }
                             >
-                              Sign Up
+                              Login
                             </button>
                           </div>
                         </div>
@@ -213,9 +188,9 @@ export default function Home() {
 <div className=" w-[372px] mt-4 mb-6 ">
 
 <div className =  'w-full border-2 border-blue-400 p-6 text-center text-[17px] font-semibold'>
-  <h1>Have an account?  
+  <h1>Dont Have an account?  
     
-    <Link href='/auth/login'><span className="ml-2 text-blue-500 cursor-pointer"> Log in</span>
+    <Link href='/'><span className="ml-2 text-blue-500 cursor-pointer"> SignUp</span>
     </Link>
     
     </h1>
