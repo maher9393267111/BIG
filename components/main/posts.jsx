34f92f3ce464bdfc6d6743c10c632f1ab8a97f4f
maher <gomemahero.com>
2022-07-12 15:28:01 +0300

    show ,empty  posts message if posts.length ==0

--- components/main/posts.jsx
+++ components/main/posts.jsx
@@ -15,13 +15,27 @@ const Posts = ({posts}) => {
 <div className='w-full grid laptop:grid-cols-3 phone:grid-cols-2'>
 
 
-{posts.map((post) => (
+{ posts.length > 0  ? 
+(
+ posts.map((post) => (
 
 <div key ={post.id} className =' '>
     <PostCart post={post}/>
 </div>
 
-))}
+))
+
+ )
+
+ : (
+
+    <div>
+        <p className=' text-red-500 text-2xl fond-bold text-center my-12'>no posts found</p>
+    </div>
+ )
+ 
+ }
+ 
 
 </div>
 
