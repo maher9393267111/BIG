4d4368bbf0b8dff060d15b7618ff68c37ade65d0
maher <gomemahero.com>
2022-07-16 00:31:52 +0300

    start comment input component

--- components/post/functionButtons.jsx
+++ components/post/functionButtons.jsx
@@ -1,94 +1,58 @@
+import { FaRegComment } from "react-icons/fa";
+import { BsHeart } from "react-icons/bs";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { Popover } from "antd";
+import {FaHeart} from 'react-icons/fa';
+const FunctionButtons = ({  likedPost,hasLiked }) => {
+  return (
+    <div>
+      <div>
+        {/* --flex icons---- */}
 
-import {FaRegComment} from 'react-icons/fa';
-import {BsHeart} from 'react-icons/bs';
-import {AiOutlineRetweet} from 'react-icons/ai';
-import { Popover   } from 'antd';
-const FunctionButtons = ({post,userinfo,likedPost}) => {
+        <div className=" flex justify-between w-[77%] mx-auto">
+          <div>
+            <p className="transition-all duration-500   hover:bg-[#CDF0EA] rounded-full text-center">
+              <div className="w-12 h-12 hover:text-[#3AB4F2] ">
+                <FaRegComment className="fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2" />
+              </div>
+            </p>
+          </div>
 
+          <div>
+            <p className="transition-all duration-500   hover:bg-[#FEE3EC] rounded-full text-center">
+              <div className="w-12 h-12 hover:text-[#FF869E] ">
+                {!hasLiked ? 
+                ( 
+                <BsHeart
+                  onClick={likedPost}
+                  className="fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2 font-bold"
+                />
+  ) : (
 
+    <div>
+         <FaHeart
+                  onClick={likedPost}
+                  className="fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2 font-bold"
+                />
 
-   
+    </div>
+  )}
+              </div>
+            </p>
+          </div>
 
-
-
-
-
-
-
-    return (
-        <div>
-
-<div>
-
-{/* --flex icons---- */}
-
-<div className=' flex justify-between w-[77%] mx-auto'>
-
-
-<div>
-    <p className='transition-all duration-500   hover:bg-[#CDF0EA] rounded-full text-center'>
-    <div className="w-12 h-12 hover:text-[#3AB4F2] ">
-            
-            <FaRegComment className= "fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2" />
-            
+          <div>
+            <p className="transition-all duration-500   hover:bg-[#CDF0EA] rounded-full text-center">
+              <div className="w-12 h-12 hover:text-green-400 relative  ">
+                <AiOutlineRetweet className="fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2" />
+              </div>
+            </p>
+          </div>
         </div>
-    </p>
-</div>
-
-
-
-<div>
-    <p className='transition-all duration-500   hover:bg-[#FEE3EC] rounded-full text-center'>
-
-    <div className="w-12 h-12 hover:text-[#FF869E] ">
-            
-            <BsHeart
-            
-            onClick={likedPost}
-            className= "fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2 font-bold" />
-            
-        </div>
-
-
-
-    </p>
-</div>
-
-
-
-<div>
-    <p className='transition-all duration-500   hover:bg-[#CDF0EA] rounded-full text-center'
-    >
-  
-
-    <div className="w-12 h-12 hover:text-green-400 relative  ">
-            
-            <AiOutlineRetweet className= "fill-current   transition-all duration-500 hover: text-3xl relative top-2 left-2" />
-            
-        </div>
-
-    </p>
-</div>
-
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-</div>
-
-
-
-        </div>
-    );
-}
+      </div>
+      <hr className="mt-2" />
+    </div>
+  );
+};
 
 export default FunctionButtons;
--- pages/post/[postid].jsx
+++ pages/post/[postid].jsx
@@ -18,7 +18,7 @@ import {
   getDocs,
   setDoc,
   serverTimestamp,
-    deleteDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import safeJsonStringify from "safe-json-stringify";
 //import Tab from '../../components/user/Tab';
@@ -28,7 +28,7 @@ import Layout from "../../components/global/layout";
 import { getAdditionalUserInfo } from "firebase/auth";
 import { useAuth } from "../../context/index";
 import { useRouter } from "next/router";
-import { ExistChat,Follow,unfollow , } from "../../utils/db";
+import { ExistChat, Follow, unfollow } from "../../utils/db";
 import { message } from "antd";
 import UserLayout from "../../components/user/userLayout";
 import PostCart from "../../components/main/postCart";
@@ -36,233 +36,150 @@ import UseronotherPosts from "../../components/post/useronotherPosts";
 import FunctionButtons from "../../components/post/functionButtons";
 import { toast } from "react-toastify";
 import { CgKey } from "react-icons/cg";
+import CommentInput from "../../components/post/Input";
 
-const Postid = ({post}) => {
+const Postid = ({ post }) => {
+  const { userinfo, handleOnotherUser } = useAuth();
 
-    const { userinfo, handleOnotherUser } = useAuth();
+  const [refresh, setRefresh] = useState(false);
 
-const [refresh, setRefresh] = useState(false);
-
-
-
-
-    const q2 = query(
-      collection(db, "InstaPosts", post?.id, "comments"),
-
-    );
-    const [comments] = useCollectionData(q2);
+  const q2 = query(collection(db, "InstaPosts", post?.id, "comments"));
+  const [comments] = useCollectionData(q2);
   //  console.log("comments---->>> is➿➿➿",comments);
 
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
 
-  
-
- 
-const [likes, setLikes] = useState([]);
-const [hasLiked, setHasLiked] = useState(false);
-
-
-    
-const fetchlikes = async () => {
-
-    const userin = await getDocs(collection(db, "InstaPosts", post?.id, "likes"));
+  const fetchlikes = async () => {
+    const userin = await getDocs(
+      collection(db, "InstaPosts", post?.id, "likes")
+    );
 
     const allLikes = [];
 
     userin.forEach((doc) => allLikes.push({ ...doc.data(), id: doc.id }));
-     console.log("🔥🔥🔥", allLikes);
+    console.log("🔥🔥🔥", allLikes);
     setLikes(allLikes);
     //  return allPosts;
-
-}
-
-
-
-
-
-
-useEffect(() => {
-
-    fetchlikes()
-
-}, [post?.id,db,refresh]);
-
-
+  };
 
   useEffect(() => {
-   
-      // ---->>> importnat to work good
-      setHasLiked(
-        likes?.findIndex((like) => like.id === userinfo.id) !== -1
-      );
-      console.log("has liked---->", hasLiked);
-  
+    fetchlikes();
+  }, [post?.id, db, refresh]);
+
+  useEffect(() => {
+    // ---->>> importnat to work good
+    setHasLiked(likes?.findIndex((like) => like.id === userinfo.id) !== -1);
+    console.log("has liked---->", hasLiked);
   }, [likes, refresh]);
 
+  const likedPost = async () => {
+    try {
+      if (userinfo.name) {
+        if (hasLiked) {
+          setRefresh(!refresh);
+          console.log(post.id, "___Delete Like____");
+          // delete doc from likes if aleready liked
+          await deleteDoc(doc(db, "InstaPosts", post.id, "likes", userinfo.id));
+          message.success(`Unliked`);
+        } else {
+          setRefresh(!refresh);
 
-
-    const likedPost = async () => {
-        try {
-          if (userinfo.name) {
-            if (hasLiked )   {
-                
-              setRefresh(!refresh);
-              console.log(post.id, "___Delete Like____");
-              // delete doc from likes if aleready liked
-              await deleteDoc(doc(db, "InstaPosts", post.id, "likes", userinfo.id));
-              message.success(`Unliked`);
-            
-            } else  {
-              setRefresh(!refresh);
-            
-              console.log(post.id, "___add Like___");
-              // add doc to likes if not already liked
-              await setDoc(doc(db, "InstaPosts", post.id, "likes", userinfo.id), {
-                username: userinfo.name,
-              });
-              //  message.success("Liked",hasLiked);
-            
-            }
-          } else {
-            
-           // message.error("Please login to like");
-          }
-        } catch (error) {
-          message.error(error.message);
+          console.log(post.id, "___add Like___");
+          // add doc to likes if not already liked
+          await setDoc(doc(db, "InstaPosts", post.id, "likes", userinfo.id), {
+            username: userinfo.name,
+          });
+          //  message.success("Liked",hasLiked);
         }
-      };
-    
+      } else {
+        // message.error("Please login to like");
+      }
+    } catch (error) {
+      message.error(error.message);
+    }
+  };
 
+  return (
+    <UserLayout userid={post?.postedbyId}>
+      {/* --grid post card and onother this post user posts--show---- */}
 
+      <div className=" grid grid-cols-12  pb-20">
+        <div className="phone:col-span-12 laptop:col-span-6  mr-8">
+          <PostCart cathide={true} post={post} />
 
+          {/* ----post likes an coments---- and create ----- */}
 
+          {/* ----likes and coments length--- */}
 
+          <div className=" phone:ml-20 laptop:ml-24   ">
+            <div className="flex gap-12 font-semibold text-lg">
+              <div className=" hover:underline cursor-pointer tg">
+                <p>{likes?.length} likes</p>
+              </div>
 
+              <div className=" hover:underline  cursor-pointer tg">
+                <p>{comments?.length} comments</p>
+              </div>
+            </div>
 
-    return (
+            {/* -----function buttons--- */}
 
-        <UserLayout
+            <div className="   my-6">
+              <FunctionButtons
+               hasLiked={hasLiked}
+               
+                likedPost={likedPost}
+              />
+            </div>
 
-        userid ={post?.postedbyId}
-           >
-
-{/* --grid post card and onother this post user posts--show---- */}
-
-<div className=" grid grid-cols-12  pb-20">
-
-{hasLiked ? 'you liket this post' : ' you have not liked this post'}
-
-<div className="phone:col-span-12 laptop:col-span-6  mr-8">
-    <PostCart cathide={true} post={post}/>
-
-{/* ----post likes an coments---- and create ----- */}
-
-{/* ----likes and coments length--- */}
-
-<div className=" phone:ml-20 laptop:ml-24   ">
-
-<div className="flex gap-12 font-semibold text-lg">
-
-<div className=" hover:underline cursor-pointer tg">
-    <p>{likes?.length} likes</p>
-</div>
-
-
-<div className=" hover:underline  cursor-pointer tg">
-    <p>
-       {comments?.length} comments
-    </p>
-</div>
-
-
-</div>
-
-
-{/* -----function buttons--- */}
-
-<div className="   my-6">
-
-
-<FunctionButtons
-likes={likes}
-comments={comments}
-userinfo={userinfo}
-post={post}
-likedPost={likedPost}
-
-/>
-
-
-
-</div>
-
-
-
-
-</div>
-
-
-</div>
-
-
-<div className=" phone:hidden laptop:block laptop:col-span-6">
-
-<UseronotherPosts   userid ={post?.postedbyId} postid={post?.id} />
-
+{/* ------wrappers----- */}
+            <div className="wrapper ">
+            
 <div>
-
-
-
-
-
-
-
-
+    <CommentInput  userinfo={userinfo}  />
 </div>
 
 
 
-</div>
+            </div>
+          </div>
+        </div>
 
+        <div className=" phone:hidden laptop:block laptop:col-span-6">
+          <UseronotherPosts userid={post?.postedbyId} postid={post?.id} />
 
-
-
-
-</div>
-
-
-
-            </UserLayout>
-    );
-}
+          <div></div>
+        </div>
+      </div>
+    </UserLayout>
+  );
+};
 
 export default Postid;
 
-
-
 export async function getServerSideProps(context) {
-    const id = context.params.postid;
-    console.log("id--->", id);
-    const snapshot = await getDoc(doc(db, "InstaPosts", id));
+  const id = context.params.postid;
+  console.log("id--->", id);
+  const snapshot = await getDoc(doc(db, "InstaPosts", id));
 
-    const userdata = snapshot.data();
-    //console.log("userdata--->", userdata);
+  const userdata = snapshot.data();
+  //console.log("userdata--->", userdata);
 
-    if (!userdata) {
-      return {
-        notFound: true,
-      };
-    }
-
-    userdata.id = snapshot.id;
-
-    //  strignfy the data
-    const post = JSON.parse(
-      safeJsonStringify({ id: snapshot.id, ...snapshot.data() }) // needed for dates
-    );
-
-
+  if (!userdata) {
     return {
-      props: { post },
+      notFound: true,
     };
   }
 
+  userdata.id = snapshot.id;
+
+  //  strignfy the data
+  const post = JSON.parse(
+    safeJsonStringify({ id: snapshot.id, ...snapshot.data() }) // needed for dates
+  );
+
+  return {
+    props: { post },
+  };
+}
